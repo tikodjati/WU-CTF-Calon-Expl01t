d476f9dfe3479023a893e5eb60346efa65bf8d84
Ms Yuipi <tikodjati08@gmail.com>
2026-01-30 14:58:10 +0700

    Create SimpleStorage.sol

---
+++ Blockchain_Hacking/8-Inheritence/SimpleStorage.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.18 <0.9.0; // stating the version
+
+contract SimpleStorage {
+   uint256 myFavoriteNumber; // inisiasi myFavoriteNumber secara default bervalue 0
+
+   // uint256[] listMyFavoriteNumber; -> dynamic array ; kalau static, di dalam kurung persegi ada valuenya.
+   // struct
+   struct otherPerson{
+       uint256 hisFavoriteNumber;
+       string name;
+   }
+
+    // Add data / value manually on struct
+    // otherPerson public Akmal = otherPerson({hisFavoriteNumber: 77, name: "Akmal"});
+    // otherPerson public Tiko = otherPerson({hisFavoriteNumber: 88, name: "Tiko"});
+
+
+    otherPerson[] public listOfOtherPerson; // [] -> empty list
+
+    // mapping
+    mapping (string => uint256) public nameToFavoriteNumber;
+
+   function store(uint256 _favoriteNumber) public virtual {
+        myFavoriteNumber = _favoriteNumber;
+   }
+
+    // view, pure : view -> return variable ; pure -> return value
+   function retrieve() public view returns(uint256){
+        return myFavoriteNumber;
+   }
+
+    // fungsi untuk menambah data orang ke dalam struct otherPerson
+    // calldata, memory, storage
+    // calldata & memory -> temporary
+    // storage -> permanent
+    // perbedaan calldata dan memory -> variabel dari memory dapat diubah, sedangkan calldata tidak
+    // contoh kasusnya adalah coba ubah value _name (yang menggunakan memory) di dalam fungsi menjadi "akmal" maka tidak eror, sedangkan jika pakai calldata tidak
+   function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfOtherPerson.push(otherPerson(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}            
+
+contract SimpleStorage2 {}
+contract SimpleStorage3 {}
+contract SimpleStorage4 {}
